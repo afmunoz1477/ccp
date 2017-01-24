1a699eeecf73cae848a5c4194f17b0b3d677592c
AndresEcheverria <andresmecheverria@gmail.com>
2017-01-23 22:42:12 -0500

    Second commit

--- ccp/app/scripts/controller/homeCtrl.js
+++ ccp/app/scripts/controller/homeCtrl.js
@@ -130,5 +130,32 @@ app.controller('homeCtrl',function($scope, $state, $location, $http) {
       });
     });
     
+    $(document).ready(function(){
+    // Add scrollspy to <body>
+      $('body').scrollspy({target: ".slidea", offset: 50});   
+
+      // Add smooth scrolling on all links inside the navbar
+      $("#footer a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+        console.log("header")
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 500, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        }  // End if
+      });
+    });
+    
 
 })
